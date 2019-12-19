43bac328882f744e7989f7964cef21117da54af8
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-12-19 15:58:54 +0530

    airport works

--- converter.c
+++ converter.c
@@ -2,24 +2,63 @@
 #define MAX 1024
 char stack[MAX];
 int top = -1;
+int indentLevel = 0;
 
 int push(char s);
 int pop();
 int isCurlyBracket(char s);
+int indent(FILE *fs);
+
+// styling functions
+
+int processTerminationSymbols(FILE*, char);
+
+int processCommaSymbols(FILE*, char);
+
+int processOpeningBraces(FILE*, char);
+
+int processClosingBraces(FILE*, char);
+
+
+
+// main caller function
 int main(){
+    // variable to collect junk data
+    int flag = 0;
 
     // file pointer to handle input and output streasms
     FILE *input;
     FILE *output;
 
+    // assign file pointers to files
     input = fopen("ugly.c", "r");
-    output = fopen("tody.c", "w");
+    output = fopen("tidy.c", "w");
 
-    //  parse charcter wise
-    char ch = getc(input);
+    //  get initial character form file
+    char ch = fgetc(input);
+
+    // loop through all characters of file
     while(ch != EOF){
-        // if ch is opening curly start
 
+        // if charcter is semi colon, handle it
+        if(processTerminationSymbols(output, ch)){
+            flag = 1;
+        }
+        else if(processCommaSymbols(output, ch)){
+            flag = 1;
+        }
+        else if(processOpeningBraces(output, ch)){
+            flag = 1;
+        }
+        else if(processClosingBraces(output, ch)){
+            flag = 1;
+        }
+        else{
+            fputc(ch, output);
+        }
+
+        // read one more charcter form stream
+        ch = getc(input);
     }
     return 0;
 }
@@ -44,9 +83,70 @@ int pop(){
 }
 
 int isCurlyBracket(char s){
-    if(s=="{" || s == "}"){
+    if(s=='{' || s == '}'){
         return 1;
     }else{
+        return 0;
+    }
+}
+
+
+int indent(FILE *fs){
+    int i;
+    for(i=0; i<indentLevel; i++){
+        fputc('\t',fs);
+    }
+}
+
+// function returns 1 when passed character was termination symbol and add newline at end
+int processTerminationSymbols(FILE* output, char ch){
+    if( ch == ';' ){
+        fputc(';', output);
+        fputc('\n', output);
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int processCommaSymbols(FILE *output, char ch){
+    if(ch == ','){
+        fputc(' ', output);
+        fputc(',', output);
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int processOpeningBraces(FILE *output, char ch){
+    if(ch=='{'){
+        // first print the curly braces to preserve code style
+        fputc('{', output);
+
+        // increase indent by one level
+        indentLevel++;
+        
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int processClosingBraces(FILE *output, char ch){
+    if(ch=='}'){
+        // print a new line chracter as closing brakets always come at newline
+        fputc('\n', output);
+        fputc('}', output);
+
+        indentLevel--;
+
+        return 1;
+    }
+    else{
         return 0;
     }
 }
