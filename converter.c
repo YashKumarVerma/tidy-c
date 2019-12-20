6edff2e97081ed541d591beac73616b2aaba27a2
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-12-20 12:56:26 +0530

    working on logs

--- converter.c
+++ converter.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
+#include<string.h>
+
 #define MAX 1024
 char stack[MAX];
 int top = -1;
 int indentLevel = 0;
+int lineStart = 0;
+int lineNumber = 0;
 
-int push(char s);
+// stack operations
+void push(char s);
 int pop();
-int isCurlyBracket(char s);
-int indent(FILE *fs);
+char getTop();
 
 // styling functions
+void indent(FILE *fs);
 
 int processTerminationSymbols(FILE*, char);
 
@@ -20,40 +25,60 @@ int processOpeningBraces(FILE*, char);
 int processClosingBraces(FILE*, char);
 
 
+// loggging functions
+void write(FILE*, char *);
 
 // main caller function
 int main(){
     // variable to collect junk data
     int flag = 0;
 
+    // dummy string to write logs
+    char str[2];
+    str[0] = ' ';
+    str[1] = '\0';
+
     // file pointer to handle input and output streasms
     FILE *input;
     FILE *output;
+    FILE *log;
 
     // assign file pointers to files
     input = fopen("ugly.c", "r");
     output = fopen("tidy.c", "w");
+    log = fopen("log.txt", "w");
+
+    // log operation starts
+    write(log, "3 Files loaded as pointers");
 
     //  get initial character form file
     char ch = fgetc(input);
+    str[0] = ch;
+    write(log, "reading character");
+    write(log, str);
 
     // loop through all characters of file
     while(ch != EOF){
 
         // if charcter is semi colon, handle it
         if(processTerminationSymbols(output, ch)){
+            // add termination symbol to stack;
+            // push(';');
             flag = 1;
         }
         else if(processCommaSymbols(output, ch)){
             flag = 1;
         }
         else if(processOpeningBraces(output, ch)){
+            // push('{');
             flag = 1;
         }
         else if(processClosingBraces(output, ch)){
             flag = 1;
         }
         else{
+
+            indent(output);
             fputc(ch, output);
         }
 
@@ -63,9 +88,9 @@ int main(){
     return 0;
 }
 
-int push(char s){
+// stack operations
+void push(char s){
     if(top = MAX){
-        return 0;
     }
     else{
         top = top + 1;
@@ -82,20 +107,27 @@ int pop(){
     }
 }
 
-int isCurlyBracket(char s){
-    if(s=='{' || s == '}'){
-        return 1;
+
+char getTop(){
+    if(top==-1){
+        return '~';
     }else{
-        return 0;
+        return stack[top];
     }
 }
 
 
-int indent(FILE *fs){
-    int i;
-    for(i=0; i<indentLevel; i++){
-        fputc('\t',fs);
+// styling operations
+void indent(FILE *fs){
+
+    if(!lineStart){
+        int i;
+        for(i=0; i<indentLevel; i++){
+            fputc('\t',fs);
+        }
     }
+
+    // // give n level indent
 }
 
 // function returns 1 when passed character was termination symbol and add newline at end
@@ -103,6 +135,7 @@ int processTerminationSymbols(FILE* output, char ch){
     if( ch == ';' ){
         fputc(';', output);
         fputc('\n', output);
+        lineStart = 0;
         return 1;
     }
     else{
@@ -124,6 +157,7 @@ int processOpeningBraces(FILE *output, char ch){
     if(ch=='{'){
         // first print the curly braces to preserve code style
         fputc('{', output);
+        fputc('\n', output);
 
         // increase indent by one level
         indentLevel++;
@@ -135,12 +169,12 @@ int processOpeningBraces(FILE *output, char ch){
     }
 }
 
-
 int processClosingBraces(FILE *output, char ch){
     if(ch=='}'){
         // print a new line chracter as closing brakets always come at newline
         fputc('\n', output);
         fputc('}', output);
+        fputc('\n', output);
 
         indentLevel--;
 
@@ -149,4 +183,15 @@ int processClosingBraces(FILE *output, char ch){
     else{
         return 0;
     }
+}
+
+// logging operations
+void write(FILE *log,int type, char str[]){
+    
+    // print lineNumber
+    lineNumber++;
+    fprintf(log, "%d", lineNumber);
+    fputc('\t', log);
+    fputs(str, log);
+    fputs("\n", log);
 }
